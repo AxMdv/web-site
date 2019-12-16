45e6b40f5e6b2d572d8c40fa5521baee69a9e579
AxMdv <56290322+AxMdv@users.noreply.github.com>
2019-12-16 23:41:09 +0300

    Update info.php

--- info.php
+++ info.php
@@ -16,9 +16,15 @@ if (isset($_SESSION['userID'])):
 ?>	
 <div class="exit-button">
 	<p><?=$_SESSION['userID'];?></p>
-	<p><a href="exit.php">Выйти</a></p>
-</div>
+	<p><a href="exit.php">Выйти</a></p>	
 
+	<?php 
+    if ($_SESSION['userID'] == "Admin123"):
+    ?>
+    <p><a href="addgood.php">Добавление продукта</a></p>
+      <?php endif; ?>
+	   
+</div>	
 <div class="content">
   <nav class="menu">
   	<ul>
@@ -26,20 +32,32 @@ if (isset($_SESSION['userID'])):
       <li class="menu-list-item"><a class="menu-item-link" href="buy.php">Купить</a></li>
       <li class="menu-list-item"><a class="menu-item-link" href="delivery.php">Доставка</a></li>
       <li class="menu-list-item"><a class="menu-item-link" href="about_us.php">О нас</a></li>
-  	  <li class="menu-list-item"><a class="menu-item-link" href="comments.php">Отзывы</a></li>
   	</ul>
   </nav>
   
   <main class="main">
-  <h2 class="greetings">Мы открылись!</h2>
+	<h2 class="greetings">Мы открылись!</h2>
+		<div>
+			<h4> Soft Goods - уникальный интернет-магазин мягких игрушек для всей семьи. </br>
+			Здесь вы можете купить игрушки на день рождения или Новый Год. </h4>
+			<h5> Наш магазин предоставляет двухнедельную гарантию на все товары. </br>
+			<img src="varanty.png"> </br>
+			В случае получения брака мы возвращаем вам деньги </br>
+			<img src="refund.jpg">
+		</div>
    
   </main>
   
   <aside class="banner">
   	<p>Покупайте наш товар по низким ценам!</p>
+	<img src="sale.jpg">
   	<p>Внимание, АКЦИЯ!!! <br>
-		При покупке двух игрушек - скидка 30%!!!</p>
-  	<p>Присоединяйтесь к нам в соц сетях</p>
+  		При покупке двух игрушек - скидка 30%!!!</p>
+		<img src="minus-thirty.jpg">
+  	<p>Присоединяйтесь к нам в соц сетях: </br>
+	Вступайте в нашу группу вконтакте: <a href="#0"> <img style="width: 5%" src="vk.png"> </a>  </br>
+	Подписывайтесь на наш инстаграм: <a href="#0"> <img style="width: 5%" src="inst.jpg"> </a> 
+	</p>
   </aside>
 </div>
 
